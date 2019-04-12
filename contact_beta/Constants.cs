902bd6edcafb4cb1afe54b35cbae16f58020807a
srb409 <32602717+srb409@users.noreply.github.com>
2019-04-12 14:07:30 -0500

    Add files via upload

---
+++ contact_beta/Constants.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace contact_beta
+{
+	public static class Constants
+	{
+		// Replace strings with your Azure Mobile App endpoint.
+		public static string ApplicationURL = @"https://contact-beta.azurewebsites.net";
+	}
+}
+
