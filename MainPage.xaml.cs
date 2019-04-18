15835cbe7becec34e3776afc97049808afbc2328
bhaselden <47121221+bhaselden@users.noreply.github.com>
2019-04-18 10:48:18 -0500

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace client_list
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            //Title label
+            Label clientList_Title = new Label();
+            clientList_Title.Text = "Client List";
+            Content = clientList_Title;
+
+            //Home button
+            Button home_BTN = new Button();
+            home_BTN.Content = "Home page";
+            Content.ChildAdded(home_BTN);
+
+            //search bar
+            private void mySearchBox_QuerySubmitted(SearchBox sender, SearchBoxQuerySubmittedEventArgs args)
+            {
+                this.Frame.Navigate(typeof(SearchResultsPage1), args.QueryText);
+            }
+            //Client names with button that goes to their profiles
+
+            //
+        }
+    }
+}
