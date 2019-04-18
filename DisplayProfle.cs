350ea7642706787f08ea0e5c17ae4b96ddf28bd6
Trevor <3761311+Trevis42@users.noreply.github.com>
2019-04-18 11:41:41 -0500

    Add files via upload
    These are all the files from the discord. We can use them to build the entire project.

---
+++ DisplayProfle.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace work
+{
+    [Activity(Label = "DisplayProfle")]
+    public class DisplayProfle : Activity
+    {
+        private Button enter;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.DisplayProfile);
+            enter = FindViewById<Button>(Resource.Id.button3);
+            enter.Click += (sender, e) =>
+            {
+                StartActivity(typeof(GetProfile));
+            };
+
+
+            // Create your application here
+        }
+    }
+}
